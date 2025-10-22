1b9d59946020362f7127f7762c0e5c65ff87cf0a
Samuel Vasquez <samyr0722@gmail.com>
2025-10-21 19:19:10 -0500

    refactoriza vide.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: ".", // importante
+  base: "/",
+  preview: {
+    allowedHosts: ["*"], // ← permite cualquier host
+    host: "0.0.0.0",
+    port: process.env.PORT || 3000
+  }
 });
