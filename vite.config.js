bc4160faee5149f1868af98cd53551fea4bb22fe
Eliel Teruel <teruel0905@gmail.com>
2025-10-24 08:42:18 -0500

    Actualización automática

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/", // 👈 usa raíz para dominios propios
-  preview: {
-    host: "0.0.0.0",
-    port: 5173,
-  },
+  base: "/", // ⚠️ raíz, no /tct-portfolio/
 });
-
