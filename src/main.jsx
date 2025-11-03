94ff7697a2a67bf1645ede2e85c93f0d8dcb7bfb
Eliel Teruel <teruel0905@gmail.com>
2025-11-03 13:42:25 -0500

    Actualización automática

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter, HashRouter } from "react-router-dom";
-import App from './App.jsx';
+import { createRoot } from "react-dom/client";
+import { HashRouter as Router } from "react-router-dom";
+import App from "./App";
 import './index.css';
 
 // Detectar entorno GitHub Pages (producción)
 const isGitHub = window.location.hostname.includes('github.io');
 const Router = isGitHub ? HashRouter : BrowserRouter;
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Router>
       <App />
