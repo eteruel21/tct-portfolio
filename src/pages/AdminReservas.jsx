b89f7e3b8d0bdbe760365e8b107e46174d86a43b
Eliel Teruel <teruel0905@gmail.com>
2025-11-04 12:58:24 -0500

    Actualización automática

--- src/pages/AdminReservas.jsx
+++ src/pages/AdminReservas.jsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from "react";
-import { motion, AnimatePresence } from "framer-motion";
+import { useState, useEffect } from "react";
+import { motion } from "framer-motion";
 import { FaSearch, FaTrashAlt, FaCalendarCheck, FaFileExcel } from "react-icons/fa";
 import * as XLSX from "xlsx";
 
--- src/pages/Reservar.jsx
+++ src/pages/Reservar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import { FaCalendarAlt, FaCheckCircle, FaTrashAlt, FaTimesCircle, FaTools } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
