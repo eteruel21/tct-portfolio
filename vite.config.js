f35eb6a64aded14b683f66f731d139333c505a63
Eliel Teruel <teruel0905@gmail.com>
2025-11-06 14:27:40 -0500

    Actualización automática

--- vite.config.js
+++ vite.config.js
@@ -35,18 +35,7 @@ export default defineConfig({
     },
     rollupOptions: {
       output: {
-        manualChunks(id) {
-          if (!id.includes("node_modules")) return;
-          if (id.includes("react")) return "vendor_react";
-          if (id.includes("react-dom")) return "vendor_react_dom";
-          if (id.includes("react-router") || id.includes("react-router-dom"))
-            return "vendor_router";
-          if (id.includes("lodash") || id.includes("lodash-es"))
-            return "vendor_lodash";
-          if (id.includes("axios")) return "vendor_axios";
-          if (id.includes("three")) return "vendor_three";
-          return "vendor_misc";
-        },
+        manualChunks: undefined,
       },
     },
   },
